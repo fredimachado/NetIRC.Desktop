589ceb88f79857807ed02d20770ef5819cc3ac27
Fredi Machado <fredisoft@gmail.com>
2021-04-06 23:48:43 +1000

    Add Event Aggregator code from Caliburn.Micro
    That's the only thing we need at the moment, and it's a pretty nice implementation

---
+++ NetIRC.Desktop/Core/IHandle.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace NetIRC.Desktop.Core
+{
+    /// <summary>
+    /// Denotes a class which can handle a particular type of message.
+    /// Source code from Caliburn.Micro
+    /// https://github.com/Caliburn-Micro/Caliburn.Micro/blob/master/src/Caliburn.Micro.Core/EventAggregator.cs
+    /// </summary>
+    /// <typeparam name = "TMessage">The type of message to handle.</typeparam>
+    // ReSharper disable once TypeParameterCanBeVariant
+    public interface IHandle<TMessage>
+    {
+        /// <summary>
+        /// Handles the message.
+        /// </summary>
+        /// <param name = "message">The message.</param>
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <returns>A task that represents the asynchronous coroutine.</returns>
+        Task HandleAsync(TMessage message, CancellationToken cancellationToken);
+    }
+}
